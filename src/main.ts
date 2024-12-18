0b3e9f4ac912580676e21aabd0f09e87a07c01b8
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-18 11:09:34 +0100

    chore(swager): move to dedicate fn and prevent deploy on production

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('Palouf')
     .setDescription('The Palouf API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, documentFactory, {
     jsonDocumentUrl: 'swagger/json',
   });
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    setupSwagger(app);
+  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
