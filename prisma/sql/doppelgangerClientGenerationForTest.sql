5d01f41cd3651e40b55726e4d574356d7cbe01f0
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-18 20:30:25 +0100

    feat(doppelganger): add script to detect doppelganger
    - Added sql script, handle by prisma in the script
    - Added test data sql script
    - Updated schema.prisma to use feature typedSql
    - Updated .dockerignore

---
+++ prisma/sql/doppelgangerClientGenerationForTest.sql
@@ -0,0 +1,12 @@
+INSERT INTO `clients` (`id`, `first_name`, `last_name`)
+VALUES
+  (100, 'Jean', 'Dupont'),
+  (101, 'Jean', 'Dupont'),
+  (102, 'Marie', 'Martin'),
+  (103, 'Marie', 'Martin'),
+  (104, 'Pierre', 'Bernard'),
+  (105, 'Jean', 'Dupont'),
+  (106, 'Jean', 'Dupont'),
+  (107, 'Jean', 'Dupont'),
+  (108, 'Isabelle', 'Thomas'),
+  (109, 'Isabelle', 'Thomas');
