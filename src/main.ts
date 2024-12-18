2d67bfde6d71eddfa02e663c4f2f4937cac2597d
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-18 11:28:22 +0100

    feat(app): handle configuration with @nestjs/config
    - Setup dedicated scope for api and database
    - Setup validation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -18,6 +19,7 @@ function setupSwagger(app: INestApplication) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.enableCors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') || 'http://localhost:3000',
@@ -26,10 +28,12 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
-  if (process.env.NODE_ENV !== 'production') {
+  if (configService.get<string>('NODE_ENV') !== 'production') {
     setupSwagger(app);
   }
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = configService.get<number>('PORT');
+
+  await app.listen(port);
 }
 bootstrap();
