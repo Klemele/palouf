5e09b7b1d329e8967c982d3cef3d0cb24dda4e75
Kevin LEMELE <lemele.kevin@gmail.com>
2024-12-18 11:28:22 +0100

    feat(app): handle configuration with @nestjs/config
    - Setup dedicated scope for api and database
    - Setup validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import apiConfiguration from './config/api.config';
+import databaseConfig from './config/database.config';
+import { validate } from './config/env.validation';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.example', '.env'],
+      load: [apiConfiguration, databaseConfig],
+      validate,
+      expandVariables: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
